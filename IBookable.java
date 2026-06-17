a6f588b13f7efc87bc010a492f9162aee888be67
dufi001 <islaeldufitimana@gmail.com>
2026-06-17 09:34:07 +0200

    Add files via upload

---
+++ IBookable.java
@@ -0,0 +1,11 @@
+/*
+ * Student 1: RegNo_FullName1 | Class: A/B/C
+ * Student 2: RegNo_FullName2 | Class: A/B/C
+ */
+package com.tickets.interfaces;
+
+public interface IBookable {
+    void buy();
+    void cancel();
+    String showTicketInfo();
+}
